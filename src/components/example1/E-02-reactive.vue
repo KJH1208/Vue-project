a18ee8dc756eb989ae5de3006fe685316c7ce0bf
Kwngus <guswnrkd12@gmail.com>
2025-11-07 02:49:53 +0900

    edit E-02-reactive.vue

--- src/components/example1/E-02-reactive.vue
+++ src/components/example1/E-02-reactive.vue
@@ -2,24 +2,19 @@
   <div>{{ fullName }}</div>
 </template>
 
-<script>
-export default {
-  name: "E02Reactive",
-  data() {
-    return {
-      firstName: "Kyungsu",
-      lastName: "Lee"
-    };
-  },
-  mounted() {
-    setTimeout(() => {
-      this.firstName = "KSL";
-    }, 2000);
-  },
-  computed: {
-    fullName() {
-      return this.firstName + " " + this.lastName;
-    }
-  }
-};
+<script setup lang="ts">
+import { ref, computed, onMounted } from 'vue';
+
+const firstName = ref("Juhyeon");
+const lastName = ref("Kang");
+
+const fullName = computed(() => {
+  return firstName.value + " " + lastName.value;
+});
+
+onMounted(() => {
+  setTimeout(() => {
+    firstName.value = "KJH";
+  }, 2000);
+});
 </script>
